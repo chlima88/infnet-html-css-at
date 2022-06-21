2f806333c4aa78df4056f87d4b0d00501cda6895
Charles Costa <chlima88@gmail.com>
2022-06-20 21:54:26 -0300

    working on hamburger menu

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 function getCards(cardName) {
 
   const grid = document.getElementById("grid1")
-
   console.log(grid.children)
-
   // const list = grid.map()
 
 }
@@ -12,19 +10,24 @@ function filterGrid(){}
 
 function handleSubmit(event) {
   event.preventDefault()
-
   console.log(event)
-
   const name = event.target[0].value
-
   const cardList = getCards(name)
-
   filterGrid(cardList)
-
 }
 
 const searchForm = document.querySelectorAll(".search-box__form")
-
-
 searchForm.forEach((form) => form.addEventListener("submit", handleSubmit ))
 
+
+
+function TogleMenu() {
+  document.querySelector(".mobile-menu-box").classList.toggle("disabled-component")
+}
+
+const openMenuButton = document.querySelector(".header-box__hamburger")
+openMenuButton.addEventListener("click", TogleMenu)
+
+
+const mobileMenuLink = document.querySelectorAll(".mobile-menu__link")
+mobileMenuLink.forEach(link => link.addEventListener("click", TogleMenu))
